399e01b0224241e1ca373f4c90f003b5ccb5e5ee
Rafael Leclerc <rafael.leclerc.rl@gmail.com>
2025-02-27 10:40:00 +0100

    Changes

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const videoContainer = document.getElementById('video-container');
     const mainContent = document.getElementById('main-content');
 
-    // Cacher la vidéo et afficher le contenu principal après 3 secondes
     setTimeout(function() {
         videoContainer.style.display = 'none';
         mainContent.style.display = 'block';
